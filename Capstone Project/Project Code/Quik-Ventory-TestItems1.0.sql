6fcab99b920cebecc5410727e2738087de24303f
Lavaracer <70068036+Lavaracer@users.noreply.github.com>
2021-04-20 15:09:22 -0500

    Add files via upload

---
+++ Capstone Project/Project Code/Quik-Ventory-TestItems1.0.sql
@@ -0,0 +1,7 @@
+insert into users (email, username, password) values ('zachary.light@usm.edu', 'zl_test', 'csc424h001');
+
+select * from users;
+
+insert into inventory (user_ID, item_ID, item_name, qty, price, dept) values (1, '501', 'lightsaber', 1, null, 'jedi');
+
+select * from inventory;
